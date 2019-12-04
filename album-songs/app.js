3514b4ab50cd52fcf0bd2ea6cbcb4dc4e14be0a8
alessioiaretti <iaretti.alessio@iisgalvanimi.edu.it>
2019-12-04 22:10:06 +0000

    api

--- album-songs/app.js
+++ album-songs/app.js
@@ -6,7 +6,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'pug');  
 
-const canzoni = require('./album-songs.json'); 
+const path = require('path');
+
+const canzoni = require('./api/album-songs.json'); 
 
 app.get('/', function (req, res)
  { 
@@ -15,6 +17,19 @@ app.get('/', function (req, res)
    canzoni: canzoni.songs 
  });
 });
+
+app.get('/api/album-songs',function(req,res){
+ res.sendFile(path.join(__dirname+'/api/album-songs.json'));
+});
+
+app.get('/song', (req, res) => {
+  const canzone = canzoni.songs.find((p) => p.song_id === req.query.song_id);
+  res.render('song', {
+    title: `About ${canzone.title}`,
+    canzone
+  });
+});
+
 
 app.listen(3000, function () {
  console.log('Apertura server web su porta 3000!!');
